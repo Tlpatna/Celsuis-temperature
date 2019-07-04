003da26d50b1eb88863cf1027aa216d3c6d1346f
Tlpatna <52210006+Tlpatna@users.noreply.github.com>
2019-07-04 23:34:10 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+void main(){
+double c,f;
+cout<<"Enter Celsuis temperature :";
+cin>>c;
+f = (1.8 * c) +32;
+	cout<<"Fahrenheit ="<<f<<endl;
+	cout<<"Now Weather in Thailand is " << (f >= 70 ? "Hot":"Cool")<<endl ;
+}
